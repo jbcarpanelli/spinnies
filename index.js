de3f1698b98529ac9d4fe42b36f9b50b5fc4a69f
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-05-08 09:11:36 -0300

    Add colors to stream text

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const readline = require('readline');
+const cliCursor = require('cli-cursor');
+const chalk = require('chalk');
 
 const MultiSpinner = {
   initialize(options = {}) {
     this.options = options;
     this.spinners = [];
     this.index = 0;
+    this.isCursorHidden = false;
     this.currentInterval = null;
   },
 
@@ -14,15 +17,15 @@ const MultiSpinner = {
     this.spinners.push({
       text,
       status: 'spinning',
+      color: 'green',
       ...options,
       index: this.index,
     });
     this.index += 1;
-    this.setSpinner();
     return { index: this.index - 1, spinners: this.spinners };
   },
 
-  setSpinner() {
+  setOrUpdateSpinners() {
     clearInterval(this.currentInterval);
     const frames = ['-', '|'];
     const interval = 80;
@@ -38,17 +41,18 @@ const MultiSpinner = {
   setStream(frame) {
     let stream = '';
     let line = '';
-    this.spinners.map(({ text, status }) => {
+    this.spinners.map(({ text, status, color }) => {
       if (status === 'spinning') {
-        line = `${frame} ${text}`;
+        line = `${chalk[color](frame)} ${chalk[color](text)}`;
       } else if (status === 'success') {
-        line = `✓ ${text}`;
+        line = `${chalk.green('✓')} ${chalk[color](text)}`;
       } else if (status === 'fail') {
-        line = `✖ ${text}`;
-      } 
+        line = `${chalk.red('✖')} ${chalk[color](text)}`;
+      } else {
+        line = `- ${chalk[color](text)}`;
+      }
       stream += `${line}\n`;
     });
-
     this.writeStream(stream);
   },
 
@@ -57,5 +61,10 @@ const MultiSpinner = {
     readline.moveCursor(process.stderr, 0, -this.spinners.length);
   },
 }
+
+process.on('SIGINT', function() {
+  MultiSpinner.spinners.map(() => console.log())
+  process.exit();
+});
 
 module.exports = MultiSpinner;
