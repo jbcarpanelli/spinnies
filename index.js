71f83c3ee61f8d92ea4b58bfb1eeb58e8559d139
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-05-08 09:38:19 -0300

    Hide cursor while spinning

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const readline = require('readline');
-const cliCursor = require('cli-cursor');
 const chalk = require('chalk');
+const cliCursor = require('cli-cursor');
 
 const MultiSpinner = {
   initialize(options = {}) {
-    this.options = options;
+    this.options = {
+      color: 'white',
+      ...options
+    };
     this.spinners = [];
     this.index = 0;
     this.isCursorHidden = false;
@@ -17,16 +20,18 @@ const MultiSpinner = {
     this.spinners.push({
       text,
       status: 'spinning',
-      color: 'green',
+      color: 'white',
       ...options,
       index: this.index,
     });
     this.index += 1;
+    this.setOrUpdateSpinners();
     return { index: this.index - 1, spinners: this.spinners };
   },
 
   setOrUpdateSpinners() {
     clearInterval(this.currentInterval);
+    this.hideCursor();
     const frames = ['-', '|'];
     const interval = 80;
     let cont = 0
@@ -56,15 +61,17 @@ const MultiSpinner = {
     this.writeStream(stream);
   },
 
+  hideCursor() {
+    if (!this.isCursorHidden) {
+      cliCursor.hide();
+      this.isCursorHidden = true;
+    }
+  },
+
   writeStream(stream) {
     process.stderr.write(stream);
     readline.moveCursor(process.stderr, 0, -this.spinners.length);
   },
 }
-
-process.on('SIGINT', function() {
-  MultiSpinner.spinners.map(() => console.log())
-  process.exit();
-});
 
 module.exports = MultiSpinner;
