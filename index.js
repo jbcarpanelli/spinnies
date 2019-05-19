0f51975179d8adfd75a381bf6472335f6f79eae8
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-05-19 14:14:43 -0300

    Change MultiSpinner object to a regular class

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ const chalk = require('chalk');
 const cliCursor = require('cli-cursor');
 const dots = require('./spinner');
 
-const { purgeInvalidOptions, isValidStatus } = require('./utils');
+const { purgeSpinnerOptions, isValidStatus } = require('./utils');
 
-const MultiSpinner = {
-  initialize(options = {}) {
+class Spinners {
+  constructor(options = {}) {
     this.options = { 
       color: 'white',
       spinnerColor: 'greenBright',
       successColor: 'green',
       failColor: 'red',
+      spinner: dots,
       ...options
     };
     this.spinners = {};
     this.isCursorHidden = false;
     this.currentInterval = null;
-    this.enabled = this.options.enabled && process.stderr.isTTY && !process.env.NODE_ENV === 'test' && !process.env.CI
-  },
+    // this.enabled = this.options.enabled && process.stderr.isTTY && !process.env.NODE_ENV === 'test' && !process.env.CI
+  }
 
   add(name, options = {}) {
     const { status, text } = options;
-    options = purgeInvalidOptions(options);
+    options = purgeSpinnerOptions(options);
     if (!name || typeof name !== 'string') throw Error('A spinner reference name must be specified');
     if (!text) throw Error('An initial spinner text must be specified');
     if (!status || !isValidStatus(status)) delete options.status;
@@ -34,9 +35,8 @@ const MultiSpinner = {
       ...options,
     };
     this.updateSpinnerState();
-
     return this.spinners[name];
-  },
+  }
 
   update(name, options = {}) {
     const { status } = options;
@@ -44,46 +44,45 @@ const MultiSpinner = {
     this.updateSpinnerState();
 
     return this.spinners[name];
-  },
+  }
 
   success(name, options = {}) {
     this.setSpinnerProperties(name, options, 'success');
     this.updateSpinnerState();
 
     return this.spinners[name];
-  },
+  }
 
   fail(name, options = {}) {
     this.setSpinnerProperties(name, options, 'fail');
     this.updateSpinnerState();
 
     return this.spinners[name];
-  },
+  }
 
   stopAll() {
-    Object.keys(this.spinners)
-      .forEach(name => {
-        const { status } = this.spinners[name];
-        if (status !== 'fail' && status !== 'success' && status !== 'non-spinnable') {
-          this.spinners[name].status = 'stopped';
-          this.spinners[name].color = 'grey';
-        }
-      });
+    Object.keys(this.spinners).forEach(name => {
+      const { status } = this.spinners[name];
+      if (status !== 'fail' && status !== 'success' && status !== 'non-spinnable') {
+        this.spinners[name].status = 'stopped';
+        this.spinners[name].color = 'grey';
+      }
+    });
     this.checkIfActiveSpinners();
 
     return this.spinners;
-  },
+  }
 
   setSpinnerProperties(name, options, status) {
-    if (!name || typeof name !== 'string') throw Error('A spinner reference name must be specified');
-    options = purgeInvalidOptions(options);
+    if (typeof name !== 'string') throw Error('A spinner reference name must be specified');
+    options = purgeSpinnerOptions(options);
     status = status || 'spinning';
 
     this.spinners[name] = { ...this.spinners[name], ...options, status };
-  },
+  }
 
   updateSpinnerState(name, options = {}, status) {
-    const { frames, interval } = dots;
+    const { frames, interval } = this.options.spinner;
     let cont = 0;
 
     clearInterval(this.currentInterval);
@@ -95,7 +94,7 @@ const MultiSpinner = {
     }, interval)
 
     this.checkIfActiveSpinners();
-  },
+  }
 
   setStream(frame) {
     let line;
@@ -113,46 +112,40 @@ const MultiSpinner = {
         }
         stream += `${line}\n`;
       });
-
     this.writeStream(stream);
-  },
+  }
 
   checkIfActiveSpinners() {
-    const { interval } = dots;
+    const { interval } = this.options.spinner;
     if (!this.hasActiveSpinners()) {
       setTimeout(() => {
         clearInterval(this.currentInterval);
         readline.moveCursor(process.stderr, 0, Object.keys(this.spinners).length);
       }, interval + 1);
     }
-  },
+  }
 
   hasActiveSpinners() {
-    return !!Object.values(this.spinners).find(({ status }) => status === 'spinning')
-  },
+    return !!Object.values(this.spinners)
+      .find(({ status }) => status === 'spinning')
+  }
 
   hideCursor() {
     if (!this.isCursorHidden) {
       cliCursor.hide();
       this.isCursorHidden = true;
     }
-  },
+  }
 
   writeStream(stream) {
-    Object.values(this.spinners)
-      .forEach(({ text }, index) => {
-        readline.moveCursor(process.stderr, text.length + 2, index);
-        readline.clearLine(process.stderr, 1);
-        readline.moveCursor(process.stderr, -(text.length + 2), -index);
-      });
+    Object.values(this.spinners).forEach(({ text }, index) => {
+      readline.moveCursor(process.stderr, text.length + 2, index);
+      readline.clearLine(process.stderr, 1);
+      readline.moveCursor(process.stderr, -(text.length + 2), -index);
+    });
     process.stderr.write(stream);
     readline.moveCursor(process.stderr, 0, -Object.keys(this.spinners).length);
-  },
+  }
 }
 
-process.on('SIGINT', function() {
-  Object.keys(MultiSpinner.spinners).forEach(() => console.log())
-  process.exit();
-});
-
-module.exports = MultiSpinner;
+module.exports = Spinners;
